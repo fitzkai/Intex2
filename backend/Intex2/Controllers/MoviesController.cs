bd0b3f357438c9639ff465c15e5fbe251d9d1848
beccaharrison <95371071+beccaharrison@users.noreply.github.com>
2025-04-07 12:40:44 -0600

    add controller

---
+++ backend/Intex2/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Intex2.API.Data;
+
+namespace Intex2.Controllers
+{
+    [Route("[controller")]
+    [ApiController]
+    public class MoviesController: ControllerBase
+    {
+        private MoviesDbContext _moviesContext;
+        public MoviesController(MoviesDbContext temp) => _moviesContext = temp;
+
+        [HttpGet("AllMovies")]
+        public IActionResult GetMovies(int pageSize = 10, int pageNum = 1)
+        {
+            var movieList = _moviesContext.Movies.ToList();
+            return (movieList);
+
+        }
+    }
+}
+
