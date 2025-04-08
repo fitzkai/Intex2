d9ab46c4f476d69b7093bec9af27f94c2371c38a
beccaharrison <95371071+beccaharrison@users.noreply.github.com>
2025-04-08 11:55:29 -0600

    add the extra db

--- backend/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f053b0d25026dae6f47188a8dcc8f2d564e34ecf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053da0849bcbb24463463e70bc83927a2ce4bbac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
