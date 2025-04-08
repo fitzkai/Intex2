6e3437df280b70a0c0cf540c30c3f9613bb593c3
fitzkai <fitzkaitlyn12@gmail.com>
2025-04-08 08:36:38 -0600

    home screen

---
+++ frontend/src/Pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import { Link, useNavigate } from "react-router-dom";
+import WelcomeBand from "../components/WelcomeBand";
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <WelcomeBand/>
+            <h1>Go no further for all your favorites!</h1>
+            <h3>Sign up today or login in with your account here!</h3>
+            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/register")}>Register</button>
+            <p>We value your privacy. Learn more about our steps to keep you safe in our {" "}<Link to='/privacy'>privacy policy</Link>.</p>
+
+            <div>
+                {/* Enter the super awesome top movie posters here! */}
+            </div>
+        </>
+    );
+}
+export default HomePage
