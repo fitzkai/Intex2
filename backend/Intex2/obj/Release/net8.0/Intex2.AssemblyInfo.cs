47b900c72c7dfebcfd874828e87e7a3dce32ffc9
fitzkai <fitzkaitlyn12@gmail.com>
2025-04-09 22:43:45 -0600

    here we go again

--- backend/Intex2/obj/Release/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/obj/Release/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c224a7dfd0524741e48d6360d196632d8af21aef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3248df71643ff9eec67f26932d2528af362f85c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
