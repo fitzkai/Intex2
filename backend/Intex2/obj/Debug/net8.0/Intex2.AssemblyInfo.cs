1619b63432708244840c8aea382a43dc6b0d7908
fitzkai <fitzkaitlyn12@gmail.com>
2025-04-10 12:19:40 -0600

    please this had better work

--- backend/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20d35a721616a0d3ebc9a01249342ce9c2bc0e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a928c3baa27faeed86baafef53c64a6cd2fdc9fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
