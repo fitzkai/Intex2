20d35a721616a0d3ebc9a01249342ce9c2bc0e3e
fitzkai <fitzkaitlyn12@gmail.com>
2025-04-10 12:05:48 -0600

    new publish folder

--- backend/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c48a5bd620a022d04cdbcdb01fb304f9c2343c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d407476083e66562ccd0d975645a769ca930bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
