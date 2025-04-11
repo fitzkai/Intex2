d47758e9585542da2d738f4acdb7a3a02227623f
fitzkai <fitzkaitlyn12@gmail.com>
2025-04-10 22:41:10 -0600

    we are victorious ... ocean

--- backend/Intex2/obj/Release/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/obj/Release/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b131123bc0fa870475f3978f714129651654e90b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e91b5e5f2541c29c7e99c4964ab895d2c98e6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
